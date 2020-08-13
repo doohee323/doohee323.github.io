054746badc00eb5dc9bfd8a416b6e4378911baef
Dewey Hong <doohee323@gmail.com>
2020-08-13 08:44:06 -0700

    Update main.js
    .

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,14 +22,14 @@ angular.module('doohee323.github.io')
 			image : 'images/6.png',
 			site : 'devops',
 			url : 'http://localhost:9001/#devops'
-		}
+		}.
 		{
 			desc : 'Senior Software Engineer',
 			name : 'Full-stack Software Engineer',
 			image : 'images/6.png',
 			site : 'se',
 			url : 'http://localhost:9001/#se'
-		},
+		}
 		];
 		
 		$state.go('init');
